7a4d36af7b0a3f83373076c320cb26050c94cb91
Aaron Kim <iamaaronkim@gmail.com>
2020-03-07 11:08:28 -0700

    updated photo

--- src/Views/Main/Main.js
+++ src/Views/Main/Main.js
@@ -1,14 +1,15 @@
-import React,{Component} from 'react'
+import React from 'react'
 import './Main.css'
 import ocean from '../../Images/ocean.jpg'
+import background from '../../Images/architecture.jpg'
 
 export default function Main(){
     return(
         <div className='main-body'>
-            <img src={ocean} alt='' className='main-pic'/>
+            <img src={background} alt='' className='main-pic'/>
             <div className='main-content'>
             <h1>Aaron Kim</h1>
-            <p>Web Development/ Mobile / Game / Blockchain</p>
+            <p>Architecure | Engineering | Drafting | Design</p>
             </div>
             
         </div>
